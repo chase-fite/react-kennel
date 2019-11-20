5ded48f722ff3d542960a727d6ff7a0fff84229f
Chase Fite <chasef7031@gmail.com>
2019-11-20 15:19:24 -0600

    finished detail practice exercise

--- src/components/location/LocationCard.js
+++ src/components/location/LocationCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 
 class LocationCard extends Component {
   render() {
@@ -8,6 +9,7 @@ class LocationCard extends Component {
           <h3>Address:</h3>
           <p>{this.props.location.address}</p>          
           <button type="button" onClick={() => this.props.deleteLocation(this.props.location.id)}>Close</button>
+          <Link to={`/locations/${this.props.location.id}`}><button>Details</button></Link>
         </div>
       </div>
     );
